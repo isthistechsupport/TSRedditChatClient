aa59dc765dce6cac73590c25ff3f682a1a124fe3
Diego Rivero <diegorivero12262@gmail.com>
2019-08-12 14:46:21 -0500

    ignore credentials

--- app.ts
+++ app.ts
@@ -4,5 +4,5 @@ import * as fs from "fs";
 
 let sb : SendBird.SendBirdInstance = new SendBird({"appId": "2515BDA8-9D3A-47CF-9325-330BC37ADA13"});
 
-let creds : {userId:String,token:String} = JSON.parse(fs.readFileSync(".\\credentials.json", "utf8"));
+let creds : {userId: String, token: String} = JSON.parse(fs.readFileSync(".\\credentials.json", "utf8"));
 
