8c7dc4524ae2c09a6cbf9c787cc246c6f7142a2d
Diego Rivero <diegorivero12262@gmail.com>
2019-08-12 13:34:15 -0500

    initial commit

---
+++ app.ts
@@ -0,0 +1,5 @@
+import * as SendBird from "sendbird";
+import * as Snoowrap from "snoowrap";
+
+let sb : SendBird.SendBirdInstance = new SendBird({"appId": "2515BDA8-9D3A-47CF-9325-330BC37ADA13"});
+
